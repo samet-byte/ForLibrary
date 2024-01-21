1b44f5acbbf559281e4fe007fb073b3d3a858219
samet-byte <sametbayat@hacettepe.edu.tr>
2024-01-21 20:37:19 +0300

    Initial commit

---
+++ image-preview/src/main/java/com/sametb/image_preview/ImagePreview.kt
@@ -0,0 +1,74 @@
+package com.sametb.image_preview
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+
+
+/*
+* ForLibrary.com.sametb.image_preview
+* Created by SAMET BAYAT 
+* on 21.01.2024 at 8:20 PM
+* Copyright (c) 2024 UNITED WORLD. All rights reserved.
+*/
+
+@Composable
+fun ImagePreview(
+    image: Painter,
+    modifier: Modifier = Modifier,
+    description: String = "",
+    contentDescription: String = "",
+    onImageClick: () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(10.dp))
+            .shadow(15.dp, RoundedCornerShape(15.dp))
+            .clickable { onImageClick() }
+    ) {
+        Image(
+            painter = image,
+            contentDescription = contentDescription,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodySmall,
+            color = Color.White,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    Brush.verticalGradient(
+                        listOf(
+                            Color.Transparent,
+                            Color.Black
+                        )
+                    )
+                )
+                .align(Alignment.BottomStart)
+                .padding(8.dp)
+        )
+    }
+}
